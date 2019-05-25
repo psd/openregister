2ee1c6f8e29db39445d30417c3c4a65af2251583
Paul Downey <psd@whatfettle.com>
2019-05-25 13:56:19 +0100

    Reinstate TravisCI for Python 3.5, 3.6, 3.7

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Database",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
