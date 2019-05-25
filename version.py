4944746bde5abe0c6782a5a63d3d2fd7da639261
Paul Downey <psd@whatfettle.com>
2019-05-25 21:45:58 +0100

    Remove trailing version from tag

--- version.py
+++ version.py
@@ -7,6 +7,7 @@ def get_version():
             subprocess.check_output("git describe --tags --match [0-9]*".split())
             .decode()
             .strip()
+            .split("-", 1)[0]
         )
     except subprocess.CalledProcessError:
         print("Unable to get version number from git tags")
@@ -14,10 +15,10 @@ def get_version():
 
 
 def next_version():
-    version = get_version().split('.')
+    version = get_version().split(".")
     version[-1] = str(int(version[-1], 10) + 1)
-    return '.'.join(version)
+    return ".".join(version)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(next_version())
