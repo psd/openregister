423bd02d7bb4cc8ebfec4154da11e18106e8e0b7
Paul Downey <psd@whatfettle.com>
2019-05-27 22:49:57 +0100

    is not

--- mirror.py
+++ mirror.py
@@ -11,30 +11,36 @@ from openregister.stores.memory import MemoryStore
 
 cache = CacheControl(requests.Session(), cache=FileCache(".cache", forever=True))
 
-def load_register(store, name, domain='register.gov.uk', protocol='https', path='download-register'):
-    url = '{protocol}://{name}.{domain}/{path}'.format(protocol=protocol, name=name, domain=domain, path=path)
+
+def load_register(
+    store, name, domain="register.gov.uk", protocol="https", path="download-register"
+):
+    url = "{protocol}://{name}.{domain}/{path}".format(
+        protocol=protocol, name=name, domain=domain, path=path
+    )
 
     response = cache.get(url)
 
     zipfile = ZipFile(BytesIO(response.content))
 
     for info in zipfile.infolist():
-        if info.filename.startswith('item/'):
+        if info.filename.startswith("item/"):
             item = Item()
-            item.json = (zipfile.open(info.filename).read().decode('utf-8'))
+            item.json = zipfile.open(info.filename).read().decode("utf-8")
             store.put(item)
+
 
 store = MemoryStore()
 
-load_register(store, name='register')
-registers = [store.item(item).get('register') for item in store.items]
+load_register(store, name="register")
+registers = [store.item(item).get("register") for item in store.items]
 
 for register in registers:
     print("loading .. ", register)
-    if not register is None:
+    if register is not None:
         load_register(store, name=register)
 
-#for item in store.items:
-    #print(item, store.item(item).json)
+# for item in store.items:
+# print(item, store.item(item).json)
 
 print(len(store.items))
